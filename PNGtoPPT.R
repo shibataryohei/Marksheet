84712c5d83f4170e117f3e534486bf8e58f57a87
Razumall <shibataryohei1202@chiba-u.jp>
2019-12-24 17:31:24 +0900

    ???

--- PNGtoPPT.R
+++ PNGtoPPT.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-PATH <- "/Users/Ryohei/Dropbox/Presentation/2019/190523_JSPS_Marksheet/Slide_JSPS2019_Marksheet" # PNGファイルのディレクトリ
+PATH <- "/Users/Ryohei/Dropbox/Presentation/2019/191207_AnnualMTG/Slide_AnnualMTG2" # PNGファイルのディレクトリ
 
 # 
 setwd(PATH) 
@@ -15,3 +15,9 @@ dir() %>%
   paste0("![](",PATH,"/", ., ")") %>%
   noquote %>% # patse()の結果から"を除去する
   cat(., sep="\n\n") # \nは改行を表す
+
+---
+  output: 
+  powerpoint_presentation:
+  reference_doc: Template_Rmd.pptx
+---
